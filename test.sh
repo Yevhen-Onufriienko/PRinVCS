400cd450cb6470456c6af8d8a35ce4b5d6ba571d
PsOlga <psyrniko@gmail.com>
2024-04-16 10:23:38 +0200

    changes_test

--- test.sh
+++ test.sh
@@ -3,3 +3,7 @@ read -p "Enter path for control: " pathchek
 read -p "Enter path for result: " pathcresult
 mrdir -p $pathcresult
 for var in $pathchek/*
+do
+echo "file name - " $var
+md5sum $pathchek/$var >> $pathcresult/$pathchek-md5sum.txt
+done
